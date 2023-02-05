d91935cde15d5d723224bdfbe7e60382d1774f4c
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-05 23:18:57 +0800

    1 file modified

--- src/app/shipping/shipping.component.spec.ts
+++ src/app/shipping/shipping.component.spec.ts
@@ -1,4 +1,4 @@
-/* import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShippingComponent } from './shipping.component';
 
@@ -20,4 +20,3 @@ describe('ShippingComponent', () => {
     expect(component).toBeTruthy();
   });
 });
- */
